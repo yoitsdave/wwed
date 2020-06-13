c494f16a297cf84e85345b8233568243b12d2d75
Judah Levy <judahjlevy@gmail.com>
2020-06-13 19:10:56 -0400

    revert some failed tries at fixing asset service

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :wwed, WwedWeb.Endpoint,
-  url: [host: "wwed.gigalixir.com", port: 4000],
+  url: [host: nil, port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,4 @@ import Config
 config :wwed, WwedWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: "wwed.gigalixir.com", port: 443]
+  url: [host: nil, port: 443]
